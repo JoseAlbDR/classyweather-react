8ad060e1c4aa75c5ebcc395851593578b676c16e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-21 09:36:13 +0200

    Create new App component for Classy Weather App

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import React from "react";
 
-class Counter extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { count: 5 };
-  }
-
-  handleDecrement() {
-    this.setState((curState) => {
-      return { count: curState.count - 1 };
-    });
-  }
-  handleIncrement() {
-    this.setState((curState) => {
-      return { count: curState.count + 1 };
-    });
   }
 
   render() {
-    const date = new Date("june 21 2027");
-    date.setDate(date.getDate() + this.state.count);
-
     return (
-      <div>
-        <button onClick={this.handleDecrement.bind(this)}>-</button>
-        <span>
-          {date.toDateString()} [{this.state.count}]
-        </span>
-        <button onClick={this.handleIncrement.bind(this)}>+</button>
+      <div className="app">
+        <h1>Classy Weather</h1>
+        <div>
+          <input type="text" placeholder="Search from location..." />
+        </div>
       </div>
     );
   }
 }
 
-export default Counter;
+export default App;
