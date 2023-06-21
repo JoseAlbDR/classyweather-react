92e22417ae05d13fb7e2ce811aecc146c0634ed7
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-21 09:28:05 +0200

    Simple component to show date and increment or decrement date with class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from "react";
 
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+  }
+
+  handleDecrement() {
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+  handleIncrement() {
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
+  }
+
   render() {
+    const date = new Date("june 21 2027");
+    date.setDate(date.getDate() + this.state.count);
+
     return (
       <div>
-        <button>-</button>
-        <span>0</span>
-        <button>+</button>
+        <button onClick={this.handleDecrement.bind(this)}>-</button>
+        <span>
+          {date.toDateString()} [{this.state.count}]
+        </span>
+        <button onClick={this.handleIncrement.bind(this)}>+</button>
       </div>
     );
   }
